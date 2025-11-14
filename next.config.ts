b6fda3dc5b8cc0e9a64fbf997aa186938e56578a
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-11-14 10:51:07 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+  },
+  images: {
+  domains: ['image.tmdb.org'],
+  },
 };
 
 export default nextConfig;
